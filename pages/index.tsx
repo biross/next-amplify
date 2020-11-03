511d75f5500210047921cc6143f0894631e22004
Brendan Ross <brendan.ross@accenture.com>
2020-11-03 17:06:22 -0600

    setup typescript and css

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./index.module.css";
+
+export default function Blogs() {
+	return (
+		<div className={styles.container}>
+			<h2>Blogs</h2>
+			<div className={styles.blogs}>Blogs go here...</div>
+		</div>
+	);
+}
