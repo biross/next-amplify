b59cee8d186fe9816ae8037fddbb419b6ae5bab3
Brendan Ross <brendan.ross@accenture.com>
2020-11-04 10:32:41 -0600

    simple nextjs app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import "../styles/globals.css";
-import "@aws-amplify/ui/dist/style.css";
 
 import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "aws-amplify-react";
 import awsExports from "../src/aws-exports";
 Amplify.configure({ ...awsExports, ssr: true });
 
@@ -10,4 +8,4 @@ function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
 
-export default withAuthenticator(MyApp, true);
+export default MyApp;
