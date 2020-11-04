c3ff90389c3dd0a21747dd481f7149696491758f
Brendan Ross <brendan.ross@accenture.com>
2020-11-04 13:02:05 -0600

    try this out?

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 
 import { Amplify } from "aws-amplify";
-import awsExports from "src/aws-exports";
+import awsExports from "../aws-exports";
 Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }) {
